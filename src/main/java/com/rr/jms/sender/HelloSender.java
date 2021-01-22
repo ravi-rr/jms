51da9bf635a1354c62225f18cb12b8b7c8c22469
Ravi Ravula <rr@test.com>
2021-01-22 18:14:12 -0500

    Implement send and receive functionality. closes #7

--- src/main/java/com/rr/jms/sender/HelloSender.java
+++ src/main/java/com/rr/jms/sender/HelloSender.java
@@ -1,12 +1,19 @@
 package com.rr.jms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rr.jms.config.JmsConfig;
 import com.rr.jms.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jms.JmsException;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -14,6 +21,7 @@ import java.util.UUID;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000) // task executor defined in TaskConfig takes care of scheduling
     public void sendMessage() {
@@ -28,5 +36,34 @@ public class HelloSender {
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message); // this makes use of the messageConverter() in JmsConfig
 
         System.out.println("message sent!");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendandReceiveMessage() throws JMSException {
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("hello")
+                .build();
+
+        Message receivedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, new MessageCreator() {
+            @Override
+            public javax.jms.Message createMessage(Session session) throws JMSException {
+                Message helloMessage = null;
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "com.rr.jms.model.HelloWorldMessage");
+
+                    System.out.println("sending hello");
+
+                    return helloMessage;
+                } catch (JsonProcessingException e) {
+                    throw new JMSException("Ah.. an exception!!");
+                }
+            }
+        });
+
+        System.out.println(receivedMsg.getBody(HelloWorldMessage.class));
     }
 }
