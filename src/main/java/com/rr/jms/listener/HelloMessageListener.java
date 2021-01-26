a9f7690643cbfcc936d7516f7bbce5d29c33ac09
Ravi Ravula <rr@test.com>
2021-01-26 16:41:49 -0500

    Revert send and receive functionality. closes #8

--- src/main/java/com/rr/jms/listener/HelloMessageListener.java
+++ src/main/java/com/rr/jms/listener/HelloMessageListener.java
@@ -2,23 +2,15 @@ package com.rr.jms.listener;
 
 import com.rr.jms.config.JmsConfig;
 import com.rr.jms.model.HelloWorldMessage;
-import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-import java.util.UUID;
-
-@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
-
-    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage message) { // spring framework will automatically map the jms message to the object
@@ -33,19 +25,4 @@ public class HelloMessageListener {
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
                        @Headers MessageHeaders headers,
                        Message message){}
-
-
-    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
-    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
-                               @Headers MessageHeaders headers,
-                               Message message) throws JMSException {
-
-        HelloWorldMessage payloadMsg = HelloWorldMessage
-                .builder()
-                .id(UUID.randomUUID())
-                .message("world!!")
-                .build();
-
-        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
-    }
 }
